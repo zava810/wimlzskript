e91c8aaba21c915017af46de03a49ffce6346dcd
vishnu.su <vishnu.su@qburst.com>
2023-01-12 22:24:32 +0530

    Add hamburger menu for mobile

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { MdDarkMode, MdLightMode } from 'react-icons/md'
 import {GiHamburgerMenu} from 'react-icons/gi'
-import { AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineClose, AiFillLinkedin, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai'
 import { useState } from 'react'
 
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true)
-  const [hamMenu, setHamMenu] = useState(true);
+  const [hamMenu, setHamMenu] = useState(false);
   return (
     <div className={darkMode ? 'dark' : ''}>
       <Head>
@@ -21,22 +21,45 @@ export default function Home() {
         <section className='py-10 px-10 min-h-screen dark:bg-neutral-900 bg'>
           <div className=' flex justify-between px-5'>
             <h1 className='text-4xl cursor-pointer dark:text-slate-200'>Vishnu</h1>
-            <div className='dark:text-slate-200 flex justify-center items-center lg:hidden cursor-pointer text-2xl' 
-              onClick={() => setHamMenu(!hamMenu)}> 
-              {hamMenu ? <GiHamburgerMenu/> : <AiOutlineClose/>}
-            </div>
-            <ul className='items-center dark:text-slate-200 hidden lg:flex'>
-              <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in'>Home</li>
-              <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in'>Blog</li>
-              <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in'>Skills</li>
-              <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in'>Contact</li>
-              <li className=' cursor-pointer text-2xl' onClick={() => setDarkMode(!darkMode)}>
-                <div className='p-2 hover:bg-neutral-700 hover:text-slate-200 dark:hover:bg-slate-200 dark:hover:text-gray-900 transition ease-in rounded-xl'>
-                  { darkMode ? <MdDarkMode/> : <MdLightMode/>}
+            <div className='flex'>
+                <ul className='items-center dark:text-slate-200'>
+                  <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in hidden lg:flex'>Home</li>
+                  <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in hidden lg:flex'>Blog</li>
+                  <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in hidden lg:flex'>Skills</li>
+                  <li className='px-10 cursor-pointer hover:text-teal-500 transition ease-in hidden lg:flex'>Contact</li>
+                  <li className=' cursor-pointer text-2xl pr-10' onClick={() => setDarkMode(!darkMode)}>
+                    <div className='p-2 hover:bg-neutral-700 hover:text-slate-200 dark:hover:bg-slate-200 dark:hover:text-gray-900 transition ease-in rounded-xl'>
+                      { darkMode ? <MdDarkMode/> : <MdLightMode/>}
+                    </div>
+                    </li>
+                </ul>
+                <div className='dark:text-slate-200 flex flex-col justify-center items-center lg:hidden cursor-pointer text-2xl' 
+                  onClick={() => setHamMenu(!hamMenu)}> 
+                  {hamMenu ? <AiOutlineClose/> : <GiHamburgerMenu/>}
                 </div>
-                </li>
-            </ul>
+              </div>
           </div>
+          {hamMenu ? <div className='py-20 flex justify-between flex-col'>
+            <div className='pb-20'>
+              <ul className='flex items-center text-center flex-col text-xl font-medium text-black dark:text-slate-200'>
+                <li className='mx-auto py-10 cursor-pointer hover:text-teal-500 transition ease-in border-b w-full'>Home</li>
+                <li className='mx-auto py-10 cursor-pointer hover:text-teal-500 transition ease-in border-b w-full'>Blog</li>
+                <li className='mx-auto py-10 cursor-pointer hover:text-teal-500 transition ease-in border-b w-full'>Skills</li>
+                <li className='mx-auto py-10 cursor-pointer hover:text-teal-500 transition ease-in border-b w-full'>Contact</li>
+              </ul>
+            </div>
+              <div className='flex items-center justify-center'>
+                <div className='px-10'>
+                  <AiFillLinkedin className='text-black dark:text-slate-200 text-xl cursor-pointer'/>
+                </div>
+                <div className='px-10'>
+                  <AiFillGithub className='text-black dark:text-slate-200 text-xl cursor-pointer'/>
+                </div>
+                <div className='px-10'>
+                  <AiOutlineInstagram className='text-black dark:text-slate-200 text-xl cursor-pointer'/>
+                </div>
+              </div>
+            </div> : null}
         </section>
       </main>
     </div>
