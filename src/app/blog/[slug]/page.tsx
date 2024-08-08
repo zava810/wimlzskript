4f24d4158c01d24c5529fe828a228cd67001f778
vishnu.su <vishnu.su@qburst.com>
2024-08-08 17:24:57 +0530

    fix responsive in blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,8 +6,8 @@ async function BlogPage({ params }: { params: { slug: string } }) {
     const slug = params.slug;
     const post = await getMediumPostById(slug);
     return (
-        <div className='w-screen min-h-screen bg-primary text-teal-500 px-96 py-20 leading-8'>
-            <h1 className='text-5xl font-semibold text-teal-400'>{post?.title}</h1>
+        <div className='w-screen min-h-screen bg-primary text-teal-500 px-20 lg:px-32 xl:px-40 2xl:px-96 py-20 leading-8'>
+            <h1 className='text-3xl lg:text-5xl font-semibold text-teal-400'>{post?.title}</h1>
             <div className='text-xs text-slate-200 justify-between py-5 px-2 border-b border-slate-700 mb-5'>
                 <div className='mb-1'>Author: {post?.author}</div>
                 <div>Published on: {getLocalDate(post?.pubDate)}</div>
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,7 @@ const Blog = async () => {
   const posts = await getMediumPosts();
   return (
     <>
-      <div className='w-screen min-h-screen bg-primary text-teal-500 px-80 py-20'>
+      <div className='w-screen min-h-screen bg-primary text-teal-500 px-10 lg:px-20 xl:px-30 2xl:px-96 py-20'>
         <h2 className='text-2xl text-slate-200 font-semibold mb-5'>Checkout my latest blogs</h2>
         <div className='py-5'>
           <BlogList posts={posts.items} />
